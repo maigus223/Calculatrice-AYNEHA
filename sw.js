498cf235a73f78a54f6db7a03c37176f35394992
maigus223 <158322662+maigus223@users.noreply.github.com>
2026-07-23 16:25:10 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,19 @@
-const CACHE_NAME = 'ayneha-calc-v13';
-const CORE_ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './ayenha-chiffres.ttf',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
-  );
-  self.skipWaiting();
+const CACHE_NAME='ayneha-calc-v14';
+const CORE_ASSETS=['./','./index.html','./manifest.json','./ayenha-chiffres.ttf','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',(event)=>{
+event.waitUntil(caches.open(CACHE_NAME).then((cache)=>cache.addAll(CORE_ASSETS)));
+self.skipWaiting();
 });
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener('activate',(event)=>{
+event.waitUntil(caches.keys().then((keys)=>Promise.all(keys.filter((k)=>k!==CACHE_NAME).map((k)=>caches.delete(k)))));
+self.clients.claim();
 });
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached || fetch(event.request).then((response) => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-        return response;
-      }).catch(() => cached);
-    })
-  );
+self.addEventListener('fetch',(event)=>{
+event.respondWith(caches.match(event.request).then((cached)=>{
+return cached||fetch(event.request).then((response)=>{
+const copy=response.clone();
+caches.open(CACHE_NAME).then((cache)=>cache.put(event.request,copy));
+return response;
+}).catch(()=>cached);
+}));
 });
